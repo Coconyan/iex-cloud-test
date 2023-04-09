2bcc0a0c6d1b04b21f6a41416175b9db2bab7368
Coconyan <San3k19@gmail.com>
2023-04-09 21:06:46 +0700

    add themeSwitcher button

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/index.scss';
 import AppRouter from './router/app-router';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 
 function App() {
+  const [theme, setTheme] = useState(true);
+
   return (
-    <div className="App dark">
-      <Header />
+    <div className={`App ${theme && 'dark'}`}>
+      <div>
+        <button className='theme-button' onClick={() => setTheme(!theme)}>change Theme</button>
+        <Header />
+      </div>
       <AppRouter />
       <Footer />
     </div>
