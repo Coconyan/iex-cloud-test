cf9a8d2ebc3d6bed06ba7ec35da87e4c63f14789
Coconyan <San3k19@gmail.com>
2023-04-09 20:36:21 +0700

    fix app location

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.scss';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { BrowserRouter } from 'react-router-dom';
-import App from './app';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
